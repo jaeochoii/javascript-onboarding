81ee51d95df5045043b0696c1ac79861a3847d88
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-26 16:47:39 +0900

    feat. problem1 calculateScore 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function problem1(pobi, crong) {}
+
+function getScore(person) {
+  const leftScore = person[0];
+  const leftValues = calculateScore(leftScore);
+  const rightScore = person[1];
+  const rightValues = calculateScore(rightScore);
 }
 
+function calculateScore(score) {
+  const hundred = Math.floor(score / 100);
+  const ten = Math.floor((score % 100) / 10);
+  const one = Math.floor((score % 100) % 10);
+
+  const plusNumber = hundred + ten + one;
+  const multiplyNumber = hundred * ten * one;
+
+  return Math.max(plusNumber, multiplyNumber);
+}
 module.exports = problem1;
