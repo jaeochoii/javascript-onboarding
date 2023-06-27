323295698553a78db9c65b12c26ec2286dc379df
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-27 13:14:33 +0900

    feat: isLengthError & isUpperCaseError 함수 생성

--- src/problem2.js
+++ src/problem2.js
@@ -9,11 +9,27 @@ function removeRepeatChar(string) {
     else {
       for (let j = i; ; j++) {
         if (string[j] === CHAR) continue;
-        else string = string.substr(i, j);
+        else {
+          string = string.substr(i, j);
+          break;
+        }
       }
     }
   }
   return string;
+}
+
+function isLengthError(string) {
+  if (string.length >= 1 && string.length <= 1000) return "Not Error";
+  else return "문자의 길이는 1 이상 1000 이하인 문자열이어야 합니다.";
+}
+
+function isUpperCaseError(string) {
+  string.map((char) => {
+    if (char !== char.toLowerCase())
+      return "문자는 소문자로 이루어져야만 합니다.";
+    else return "Not Error";
+  });
 }
 
 module.exports = problem2;
