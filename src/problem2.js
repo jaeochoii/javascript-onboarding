fdb464453d57edcc683a99b1378787a5481e84ed
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-27 13:07:11 +0900

    feat: problem2 removeReapeatChar 함수 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeRepeatChar(cryptogram);
+}
+
+function removeRepeatChar(string) {
+  for (let i = 0; i < string.length; i++) {
+    const CHAR = string[i];
+    if (string[i] !== string[i + 1]) continue;
+    else {
+      for (let j = i; ; j++) {
+        if (string[j] === CHAR) continue;
+        else string = string.substr(i, j);
+      }
+    }
+  }
+  return string;
 }
 
 module.exports = problem2;
