e7046dde3369e5b3a678acd9e29760c9fe96ffff
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-29 14:34:43 +0900

    feat: problem4 changeWord 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,9 @@
-function problem4(word) {}
+function problem4(word) {
+  const alphabet = Array.from({ length: 26 }, (index) =>
+    String.fromCharCode(index + 65)
+  );
+  const reverseAlphabet = alphabet.reverse();
+}
 
 function isLengthError(word) {
   const leastLength = 0;
@@ -8,6 +13,16 @@ function isLengthError(word) {
     return "word의 길이는 1이상 1,000 이하입니다.";
   }
   return "Not Error";
+}
+
+function changeWord(word, arr, reverseArr) {
+  const array = word.split("");
+  let resultArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === " ") resultArr.push(" ");
+    let foundWord = arr.indexOf(array[i]);
+    resultArr.push(reverseArr[foundWord]);
+  }
 }
 
 module.exports = problem4;
