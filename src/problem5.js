674748b5db806568e3b55f9426c3a802fdeecb6d
jaeochoiii <jaeochoiii0716@inha.edu>
2023-07-01 20:06:52 +0900

    feat: problem5 결과 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,22 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const BILL_UNIT_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const BILL_LENGTH = BILL_UNIT_LIST.length;
+  let billUnitCount = new Array(BILL_LENGTH).fill(0);
+  const result = countList(money, BILL_UNIT_LIST, billUnitCount);
+  return result;
+}
+
+function countList(money, unitList, countList) {
+  while (money > 0) {
+    for (let billIndex = 0; billIndex < unitList.length; billIndex++) {
+      if (money >= unitList[billIndex]) {
+        money -= unitList[billIndex];
+        countList[billIndex]++;
+        billIndex--;
+      } else continue;
+    }
+  }
+  return countList;
 }
 
 module.exports = problem5;
