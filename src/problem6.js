f3cb1c9e696fbb606b2e3322163ed29f995b2301
jaeochoiii <jaeochoiii0716@inha.edu>
2023-07-02 19:42:46 +0900

    feat: isRepeatableNickname 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -6,4 +6,19 @@ function sortEmailArray(email_set) {
   return emailArray;
 }
 
+function isRepeatableNickname(user1, user2, forms) {
+  let findStr = "";
+  nickName1 = forms[user1][1];
+  nickName2 = forms[user2][1];
+
+  for (let index = 0; index < nickName1.length - 1; index++) {
+    findStr = nickName1.substring(index, index + 2);
+
+    if (nickName2.includes(findStr)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 module.exports = problem6;
