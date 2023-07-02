2e20195221c05e1c5fd6f17e869ca4de62e16708
jaeochoiii <jaeochoiii0716@inha.edu>
2023-07-02 19:57:44 +0900

    feat: problem6 완성

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,26 @@
-function problem6(forms) {}
+function problem6(forms) {
+  const result = [];
+  const userNumber = forms.length;
+  let email_set = new Set();
+
+  if (userNumber < 2) return result;
+
+  let repeatableArr = Array.from({ length: userNumber }, () => 0);
+
+  for (let i = 0; i < userNumber; i++) {
+    for (let j = i + 1; j < userNumber; j++) {
+      if (repeatableArr[i] === 1 && repeatableArr[j] === 1) continue;
+      if (isRepeatableNickname(i, j, forms)) {
+        repeatableArr[i] = 1;
+        repeatableArr[j] = 1;
+        email_set.add(forms[i][0]);
+        email_set.add(forms[j][0]);
+      }
+    }
+  }
+  result = sortEmailArray(email_set);
+  return result;
+}
 
 function sortEmailArray(email_set) {
   let emailArray = Array.from(email_set);
